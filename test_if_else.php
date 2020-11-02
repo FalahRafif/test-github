f2c3744d3391ae9b214fb336d6ffe66b82e5caef
FalahRafif <61583572+FalahRafif@users.noreply.github.com>
2020-11-02 09:47:19 +0700

    create choose method

--- test_if_else.php
+++ test_if_else.php
@@ -1,1 +1,18 @@
+<?php
 
+class if_else {
+  
+  public function choose($choose) // choose format "choose_1-choose_2"
+  {
+    $chooses = explode("-", $choose);
+    $totalChooses = count($chooses);
+    $iChoose = rand(0, $totalChoose);
+    $hereMyChoose = "i choose " . $chooses[$ichoose];
+    
+    return $hereMyChoose;
+  }
+  
+}
+    
+      
+              
